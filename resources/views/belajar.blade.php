c55fa741a51d43bf99af9272e4ffdb38af0f0297
BaaLLana <baallana5055@gmail.com>
2024-02-21 14:54:03 +0700

    halo

---
+++ resources/views/belajar.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini View Pertama Saya</title>
+</head>
+
+<body>
+    Belajar Controller Laravel Mudah
+</body>
+
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Michael aslinya Iqbal</title>
+</head>
+<body>
+    
+First view in laravel <br>
+Perkenalkan nama saya Muhammad Iqbal Maulana
+
+</body>
+</html>
